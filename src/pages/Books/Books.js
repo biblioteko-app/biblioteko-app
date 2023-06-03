bf1536bcb658cd50c569229e073eea09d83ae6da
Junior <69211250+juniorjse@users.noreply.github.com>
2023-06-02 23:26:31 -0300

    update 23:26

--- src/pages/Books/Books.js
+++ src/pages/Books/Books.js
@@ -7,17 +7,19 @@ import { MdAdd } from 'react-icons/md'
 const Books = () => {
   return (
     <>
-      <div className="books">
-        <BooksCarousel title="Livros favoritados" />
-      </div>
+      <div className="container">
+        <div className="books">
+          <BooksCarousel title="Livros favoritados" />
+        </div>
 
-      <div className="books">
-        <BooksCarousel title="Leituras em andamento" />
-      </div>
-      <div className="add-button">
-        <Link to="/addBook">
-          <MdAdd className="add-icon" />
-        </Link>
+        <div className="books">
+          <BooksCarousel title="Leituras em andamento" />
+        </div>
+        <div className="add-button">
+          <Link to="/addBook">
+            <MdAdd className="add-icon" />
+          </Link>
+        </div>
       </div>
     </>
   )
