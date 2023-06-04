ba9dc57902857085c5b3653cafe5182d99d0ee9c
Junior <69211250+juniorjse@users.noreply.github.com>
2023-06-04 12:13:02 -0300

    update carousel
    ocorre um erro, pois não consegue encontrar a rota no back

--- src/components/BooksCarousel/BooksCarousel.jsx
+++ src/components/BooksCarousel/BooksCarousel.jsx
@@ -1,36 +1,26 @@
-import React from 'react'
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-
 import 'swiper/css'
 import 'swiper/css/grid'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
 import './BooksCarousel.css'
-
 import { Pagination, Navigation } from 'swiper'
 import BookCard from '../BookCard/BookCard'
 import { getBooks } from '../../services/BookService'
 
 export default function BooksCarousel(options) {
-  const [slidesPerView, setSlidesPerView] = useState(0);
-  const carouselRef = useRef();
+  const [slidesPerView, setSlidesPerView] = useState(0)
+  const carouselRef = useRef()
 
-  useEffect(
-    () => {
-      setSlidesPerView(Math.round((window.innerWidth * 0.8) / 175))
-    }
-  )
+  useEffect(() => {
+    setSlidesPerView(Math.round((window.innerWidth * 0.8) / 175))
+  }, [])
 
-  // Math.round(carouselRef.current.offsetWidth / 175)
-  console.log(slidesPerView)
   const rows = Number(options['rows'])
-  
-  
-
-  const books = options.books;
-  const addToSchoolClass = options.addToSchoolClass;
-  const schoolClass = options.schoolClass;
+  const books = options.books
+  const addToSchoolClass = options.addToSchoolClass
+  const schoolClass = options.schoolClass
 
   // useEffect(() => {
   //   async function b() {
@@ -44,34 +34,30 @@ export default function BooksCarousel(options) {
       <div className="title">{options['title']}</div>
       <div className="carousel">
         <Swiper
-          slidesPerView={"auto"}
+          slidesPerView="auto"
           spaceBetween={15}
           navigation={true}
           modules={[Navigation]}
           className="mySwiper"
         >
-          {
-            books.map(book => {
-              return (
-                <SwiperSlide>
-                  <BookCard
-                    book={{
-                      title: book.title,
-                      author: book.author,
-                      progress: book.progress,
-                      rating: book.rating,
-                      edition: book.edition,
-                      gender: book.gender,
-                      synopsis: book.synopsis,
-                      pageNumber: book.pageNumber
-                    }}
-                    addToSchoolClass={addToSchoolClass}
-                    schoolClass={schoolClass}
-                  />
-                </SwiperSlide>      
-              )
-            })
-          }
+          {books.map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard
+                book={{
+                  title: book.title,
+                  author: book.author,
+                  progress: book.progress,
+                  rating: book.rating,
+                  edition: book.edition,
+                  gender: book.gender,
+                  synopsis: book.synopsis,
+                  pageNumber: book.pageNumber
+                }}
+                addToSchoolClass={addToSchoolClass}
+                schoolClass={schoolClass}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </>
